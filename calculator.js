38f7d192bdb19cd0e5ababaa553f5493eda29bdc
topsyakins <topsyakins@gmail.com>
2022-06-26 19:38:41 +0100

    initial commit

---
+++ calculator.js
@@ -0,0 +1,14 @@
+var num1 = parseInt (prompt('num1: '))
+var opr = prompt("operator: - / * + ");
+var num1 = parseInt (prompt('num2: '))
+
+if (opr === '+') {alert(num1 + num2);
+console.log (num1 + num2)}
+else if (opr === '+'){alert(num1 + num2);
+console.log (num1 + num2)}
+else if (opr === '-'){alert(num1 - num2);
+console.log (num1 - num2)}
+else if (opr === '*'){alert(num1 * num2);
+console.log (num1 * num2)}
+else if (opr === '/'){alert(num1 / num2);
+console.log (num1 / num2)}
